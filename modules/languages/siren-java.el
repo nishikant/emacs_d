3341ec5fa71deca31b043cc49d44a1033eb0b149
gattu <gattusevalkar@gmail.com>
2024-01-11 23:32:49 +0530

    added java module

---
+++ modules/languages/siren-java.el
@@ -0,0 +1,17 @@
+;;; siren-java.el --- jimeh's Emacs Siren: go-mode configuration.
+
+;;; Commentary:
+
+;; Basic configuration for go-mode.
+
+;;; Code:
+
+
+(add-to-list 'lsp-enabled-clients 'jdtls)
+
+(use-package java-snippets)
+
+(setq c-basic-offset 4)
+
+(provide 'siren-java)
+;;; siren-java.el ends here
