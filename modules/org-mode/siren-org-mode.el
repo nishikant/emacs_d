8a33874d0f99c6df142d66c10b639d83afd737c4
gattu <gattusevalkar@gmail.com>
2024-01-11 22:14:31 +0530

    Rust changes

--- modules/org-mode/siren-org-mode.el
+++ modules/org-mode/siren-org-mode.el
@@ -86,7 +86,8 @@
   :preface
   (defun siren-org-mode-setup ()
     (setq fill-column 80
-          whitespace-action '(auto-cleanup))
+          whitespace-action '(auto-cleanup)
+          tab-width 8)
 
     (setcar (nthcdr 4 org-emphasis-regexp-components) 20)
     (org-set-emph-re 'org-emphasis-regexp-components
