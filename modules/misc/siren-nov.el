cb4e180e6f0b8003b901fc016bd92687c1e5ec5f
gattu <gattusevalkar@gmail.com>
2024-01-31 00:03:32 +0530

    added epub reader nov

--- modules/misc/siren-nov.el
+++ modules/misc/siren-nov.el
@@ -10,10 +10,11 @@
 
 (straight-use-package '(nov-xwidget :host github
                                     :repo "chenyanming/nov-xwidget"
-                                    :branch "main"))
+                                    :branch "main"
+                                    :after nov))
 
 (define-key nov-mode-map (kbd "o") 'nov-xwidget-view)
 (add-hook 'nov-mode-hook 'nov-xwidget-inject-all-files)
-
+(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 (provide 'siren-nov)
 ;;; siren-nov.el ends here
