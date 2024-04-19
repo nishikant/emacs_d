e2a43fd164cbdec2784ce94948c581622f388f77
Gattu <nishikant.sevalkar@gmail.com>
2024-04-19 10:59:55 +0530

    Added Jira and commify

--- core/siren-core-modules.el
+++ core/siren-core-modules.el
@@ -246,6 +246,8 @@
 (require 'siren-yaml)
 (require 'siren-pdf)
 (require 'siren-nov)
+(require 'siren-jira)
+(require 'siren-commify)
 
 (provide 'siren-core-modules)
 ;;; siren-core-modules.el ends here
