b41395310c6e8a6e3b518e7ed398155f08e7267a
Gattu <gattu@Gattus-MacBook-Pro.local>
2025-10-05 22:42:28 +0530

    mermaid fixes

--- modules/languages/siren-mermaid.el
+++ modules/languages/siren-mermaid.el
@@ -2,14 +2,15 @@
 
 ;;; Commentary:
 
-;; Basic configuration for mermaid-mode.
+;; Basic configuration for mermaid-mode and ob-mermaid.
 
 ;;; Code:
 
 (require 'siren-prog-mode)
+(require 'siren-org-mode)
 
 (use-package mermaid-mode
-  :mode "\\.mermaid\\'" "\\.mmd\\'"
+  :mode ("\\.mermaid\\'" "\\.mmd\\'")
 
   :hook
   (mermaid-mode . siren-mermaid-mode-setup)
@@ -20,21 +21,28 @@
 
 (use-package ob-mermaid
   :straight '(ob-mermaid :type git :host github :repo "arnm/ob-mermaid"
-                         :files (:defaults "*.el" ))
+                         :files (:defaults "*.el"))
 
   :after org
   :commands (org-babel-execute:mermaid)
+
+  :hook
+  (org-mode . siren-ob-mermaid-setup)
+
+  :preface
+  (defun siren-ob-mermaid-setup ()
+    (require 'ob-mermaid))
+
   :init
   (add-to-list 'org-src-lang-modes '("mermaid" . mermaid))
+
   :config
+  (setq ob-mermaid-cli-path "mmdc")
   ;; Uncomment the following line to use ob-mermaid without the need to
   ;; install mermaid-cli globally (requires Node.js). This will install it
-  ;; locally in your `straight-base-dir'.
+  ;; locally in your `straight-base-dir`.
   ;; (setq ob-mermaid-cli-path (expand-file-name "node_modules/.bin/mmdc" straight-base-dir))
-  (setq ob-mermaid-cli-path "mmdc")
   )
-
-
 
 (provide 'siren-mermaid)
 ;;; siren-mermaid.el ends here
