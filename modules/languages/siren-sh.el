ea0cdab7bfaf904b649485f838cb523e0a285429
Gattu <gattu@Gattus-MacBook-Pro.local>
2025-10-05 18:57:47 +0530

    sh lsp and install fix

--- modules/languages/siren-sh.el
+++ modules/languages/siren-sh.el
@@ -6,26 +6,30 @@
 
 ;;; Code:
 
+
 (use-package sh-script
   :straight (:type built-in)
   :mode
-  ("/\\.env\\'" . sh-mode)
-  ("/\\.env\\." . sh-mode)
-  ("/\\.envrc\\'" . sh-mode)
-  ("/\\.envrc\\." . sh-mode)
-  ("\\.zsh\\'" . sh-mode)
-  ("/zshenv\\'" . sh-mode)
-  ("/zshrc\\'" . sh-mode)
-  ("\\.tmux\\'" . sh-mode)
-  ("\\.tmuxsh\\'" . sh-mode)
-  ("\\.tmuxtheme\\'" . sh-mode)
+  ("\\.env\\'" . sh-mode)              ; .env files
+  ("\\.env\\..*\\'" . sh-mode)         ; .env.* files
+  ("\\.envrc\\'" . sh-mode)            ; .envrc files
+  ("\\.zsh\\'" . sh-mode)              ; .zsh files
+  ("\\.zshrc\\'" . sh-mode)            ; .zshrc files
+  ("\\.zshenv\\'" . sh-mode)           ; .zshenv files  
+  ("\\.zprofile\\'" . sh-mode)         ; .zprofile files
+  ("\\.zlogin\\'" . sh-mode)           ; .zlogin files
+  ("\\.zlogout\\'" . sh-mode)          ; .zlogout files
+  ("\\.tmux\\'" . sh-mode)             ; .tmux files
+  ("\\.tmuxsh\\'" . sh-mode)           ; .tmuxsh files
+  ("\\.tmuxtheme\\'" . sh-mode)        ; .tmuxtheme files
+  ("/etc/zsh/.*\\'" . sh-mode)         ; files in /etc/zsh/ directory
 
   :general
   (:keymaps 'sh-mode-map
             "RET" 'newline-and-indent)
 
   :hook
-  (sh-mode . siren-sh-mode-setup)
+  (sh-mode . lsp-deferred)
 
   :custom
   (sh-basic-offset 2)
@@ -34,53 +38,15 @@
 
   :preface
   (defun siren-sh-mode-setup ()
-    (setq-local tab-width 2)))
+    (setq-local tab-width 2))
 
-(use-package lsp-bash
-  :straight lsp-mode
-
-  :hook
-  (sh-mode . siren-lsp-bash-mode-setup)
-
-  :preface
-  (defun siren-lsp-bash-mode-setup ()
-    (when (member sh-shell '(bash sh))
-      (if (fboundp 'lsp-deferred)
-          (lsp-deferred))
-      (if (fboundp 'tree-sitter-mode)
-          (tree-sitter-mode t))))
-
-  :config
-  ;; Create custom lsp-client for shellcheck diagnostics via efm-langserver.
-  (when (and (executable-find "efm-langserver")
-             (executable-find "shellcheck"))
-    (lsp-register-custom-settings
-     '(("shellcheck.rootMarkers" [".git/"])
-       ("shellcheck.languages"
-        ((shellscript . [((lintCommand . "shellcheck -f gcc -x -")
-                          (lintStdin . t)
-                          (lintSource . "shellcheck")
-                          (lintFormats . ["%f:%l:%c: %trror: %m"
-                                          "%f:%l:%c: %tarning: %m"
-                                          "%f:%l:%c: %tote: %m"]))])))))
-    (lsp-register-client
-     (make-lsp-client :new-connection (lsp-stdio-connection
-                                       '("efm-langserver"))
-                      :priority 0
-                      :activation-fn #'lsp-bash-check-sh-shell
-                      :initialized-fn
-                      (lambda (workspace)
-                        (with-lsp-workspace workspace
-                          (lsp--set-configuration
-                           (gethash "shellcheck"
-                                    (lsp-configuration-section "shellcheck")))))
-                      :server-id 'shellcheck
-                      :add-on? t))))
+  (with-eval-after-load 'lsp-mode
+    (add-to-list 'lsp-language-id-configuration '(sh-mode . "shellscript"))
+    (add-to-list 'lsp-enabled-clients 'bash-language-server)))
 
 (use-package shfmt
   :hook
   (sh-mode . shfmt-on-save-mode)
-
   :custom
   (shfmt-arguments '("-i" "2" "-ci" "-sr")))
 
